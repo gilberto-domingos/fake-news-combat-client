b38d556c8157743583fe806e0b7a41f9bf0f652f
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-02-11 04:24:52 -0300

    feat: navbar (#10)

---
+++ src/app/components/testcomp/testcomp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testcomp',
+  imports: [],
+  templateUrl: './testcomp.html',
+  styleUrl: './testcomp.scss',
+})
+export class Testcomp {
+
+}
