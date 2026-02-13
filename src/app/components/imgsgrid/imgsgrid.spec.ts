04f57e35c38f32c095d2682c8a391654913d50cd
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-02-13 14:05:57 -0300

    chore: update change grid (#16)

--- src/app/components/imgsgrid/imgsgrid.spec.ts
+++ src/app/components/imgsgrid/imgsgrid.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Testcomp } from './testcomp';
+import { Imgsgrid } from './imgsgrid';
 
-describe('Testcomp', () => {
-  let component: Testcomp;
-  let fixture: ComponentFixture<Testcomp>;
+describe('Imgsgrid', () => {
+  let component: Imgsgrid;
+  let fixture: ComponentFixture<Imgsgrid>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Testcomp]
+      imports: [Imgsgrid]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Testcomp);
+    fixture = TestBed.createComponent(Imgsgrid);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
