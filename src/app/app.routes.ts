79f628579600253dfcecbba382e047270233de96
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-02-13 14:05:57 -0300

    chore: navbar , grid layout (#13)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./components/home/home').then((m) => m.Home),
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./components/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'grid',
+    loadComponent: () => import('./components/imgsgrid/imgsgrid').then((m) => m.Imgsgrid),
+  },
 ];
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+  isImage?: boolean;
+  title?: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
