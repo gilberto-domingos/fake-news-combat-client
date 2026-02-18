111fb8b9a2cb4514a009a2a794cc294c370a170f
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-02-18 01:21:23 -0300

    feat: new routes (#25)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,25 @@ export const routes: Routes = [
     loadComponent: () => import('./components/home/home').then((m) => m.Home),
   },
   {
+    path: 'home',
+    loadComponent: () => import('./components/home/home').then((m) => m.Home),
+  },
+  {
     path: 'sigup',
     loadComponent: () => import('./components/sigup/sigup').then((m) => m.Sigup),
   },
   {
     path: 'sigin',
     loadComponent: () => import('./components/sigin/sigin').then((m) => m.Sigin),
+  },
+  {
+    path: 'conditions',
+    loadComponent: () =>
+      import('./components/termsconditions/termsconditions').then((m) => m.Termsconditions),
+  },
+  {
+    path: 'privacy',
+    loadComponent: () =>
+      import('./components/privacynotice/privacynotice').then((m) => m.Privacynotice),
   },
 ];
