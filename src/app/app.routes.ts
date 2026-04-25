0cdaaf2555bdf0d2533969e48dafcf83cf1daf16
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-04-25 03:43:28 -0300

    refactor: wrapper (#144)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/home/home').then((m) => m.Home),
+    loadComponent: () => import('./features/land/land').then((m) => m.Land),
+  },
+  {
+    path: 'land',
+    loadComponent: () => import('./features/land/land').then((m) => m.Land),
   },
   {
     path: 'home',
---
+++ src/app/features/land/land.ts
@@ -0,0 +1,32 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { HealthzService } from '../home/healthz-service';
+
+@Component({
+  selector: 'app-land',
+  imports: [MatCardModule],
+  templateUrl: './land.html',
+  styleUrl: './land.scss',
+})
+export class Land implements OnInit {
+  isLoading = true;
+
+  healthzService = inject(HealthzService);
+
+  ngOnInit(): void {
+    const start = performance.now();
+
+    this.healthzService.checkHealthz().subscribe({
+      next: () => {
+        console.log('Healthz backend :');
+        const duration = performance.now() - start;
+        console.log(`Time to check healthz OK :', ${duration}ms`);
+      },
+      error: () => {
+        console.warn('Check healthz backend is no wakeup, Warning !');
+        const duration = performance.now() - start;
+        console.warn(`Error check healthz backend after ${duration} ms`);
+      },
+    });
+  }
+}
