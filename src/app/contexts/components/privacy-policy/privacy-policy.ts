30d5f4da58f16af7b5ba903d397d444454d26557
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-05-16 01:03:49 -0300

    feat: privacy, terms condition (#172) (#132)

--- src/app/contexts/components/privacy-policy/privacy-policy.ts
+++ src/app/contexts/components/privacy-policy/privacy-policy.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { TranslocoService } from '@jsverse/transloco';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [MatCardModule, MatDividerModule],
+  imports: [MatCardModule, MatDividerModule, TranslocoDirective],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
-export class PrivacyPolicy {}
+export class PrivacyPolicy {
+  private translocoService = inject(TranslocoService);
+}
--- src/app/contexts/components/terms-conditions/terms-conditions.ts
+++ src/app/contexts/components/terms-conditions/terms-conditions.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { TranslocoService } from '@jsverse/transloco';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-terms-conditions',
-  imports: [],
+  imports: [MatCardModule, MatDividerModule, TranslocoDirective],
   templateUrl: './terms-conditions.html',
   styleUrl: './terms-conditions.scss',
 })
 export class TermsConditions {
-
+  private translocoService = inject(TranslocoService);
 }
