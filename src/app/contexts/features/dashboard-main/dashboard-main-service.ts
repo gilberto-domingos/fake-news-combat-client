57a6a4c1440117558f247b6722529067007ec9a2
gilberto-domingos <gilberto1domingos1@outlook.com>
2026-05-27 18:22:18 -0300

    feat: table paginator (#193)

--- src/app/contexts/features/dashboard-main/dashboard-main-service.ts
+++ src/app/contexts/features/dashboard-main/dashboard-main-service.ts
@@ -1,9 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
-
 import { environment } from 'src/environments/environment';
-
 import { AnalyticsResDto } from 'app/core/analytics-access/analytics.res.dto';
 
 @Injectable({
@@ -11,11 +9,22 @@ import { AnalyticsResDto } from 'app/core/analytics-access/analytics.res.dto';
 })
 export class DashboardMainService {
   private http = inject(HttpClient);
-
   private apiUrl = `${environment.apiUrl}/analytics_access/find_all`;
 
-  getAll(): Observable<AnalyticsResDto[]> {
-    return this.http.get<AnalyticsResDto[]>(this.apiUrl).pipe(catchError(this.handleError));
+  getAll(params?: {
+    page?: number;
+    pageSize?: number;
+    search?: string;
+  }): Observable<AnalyticsResDto[]> {
+    let httpParams = new HttpParams();
+
+    if (params?.page) httpParams = httpParams.set('page', params.page);
+    if (params?.pageSize) httpParams = httpParams.set('pageSize', params.pageSize);
+    if (params?.search) httpParams = httpParams.set('search', params.search);
+
+    return this.http
+      .get<AnalyticsResDto[]>(this.apiUrl, { params: httpParams })
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: any) {
@@ -26,9 +35,9 @@ export class DashboardMainService {
     if (error.error?.detail) {
       message = error.error.detail;
     } else if (error.status === 0) {
-      message = 'Server unavailable';
+      message = 'Server unavailablel';
     } else if (error.status === 404) {
-      message = 'User not found';
+      message = 'User not Found';
     }
 
     return throwError(() => new Error(message));
--- src/app/contexts/features/dashboard-main/dashboard-main.ts
+++ src/app/contexts/features/dashboard-main/dashboard-main.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,11 +6,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { DashboardMainService } from './dashboard-main-service';
 import { AnalyticsResDto } from './../../../core/analytics-access/analytics.res.dto';
 import { signal } from '@angular/core';
+
+import { ViewChild, AfterViewInit, inject, DestroyRef, OnInit } from '@angular/core';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatSort, MatSortModule, SortDirection } from '@angular/material/sort';
+import { merge, Observable, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-main',
@@ -24,32 +31,31 @@ import { signal } from '@angular/core';
     MatCardModule,
     MatInputModule,
     MatTableModule,
+    MatPaginatorModule,
   ],
+  standalone: true,
   templateUrl: './dashboard-main.html',
   styleUrl: './dashboard-main.scss',
-  standalone: true,
 })
 export class DashboardMain implements OnInit {
   private dashboardService = inject(DashboardMainService);
   private destroyRef = inject(DestroyRef);
 
   displayedColumns: string[] = [
-    'id',
     'sessionId',
     'route',
     'timestamp',
-    'user_agent',
     'language',
-    'platform',
-    'screen_width',
-    'screen_height',
     'timezone',
-    'fingerprint',
     'ip_address',
     'country',
     'bot_detection',
     'authenticate_user_id',
   ];
+
+  page = signal(1);
+  pageSize = signal(5);
+  totalItems = signal(0);
 
   analytics = signal<AnalyticsResDto[]>([]);
   loading = signal(false);
@@ -65,14 +71,17 @@ export class DashboardMain implements OnInit {
     this.errorMessage.set(null);
 
     this.dashboardService
-      .getAll()
+      .getAll({
+        page: this.page(),
+        pageSize: this.pageSize(),
+        search: '',
+      })
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: (response: AnalyticsResDto[]) => {
           console.log('INFORMAÇÕES DO BANCO', response);
-
           this.analytics.set(response);
-
+          this.totalItems.set(100);
           this.loading.set(false);
         },
 
@@ -82,5 +91,13 @@ export class DashboardMain implements OnInit {
           this.loading.set(false);
         },
       });
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.page.set(event.pageIndex + 1);
+
+    this.pageSize.set(event.pageSize);
+
+    this.loadAnalytics();
   }
 }
